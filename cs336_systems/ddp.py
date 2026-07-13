22bd3571178fe7229b9ca651b59d01bd72092875
wooloo_lover <wooloo.loverr@gmail.com>
2026-07-13 16:39:20 +0800

    修改为backward overlap版本的ddp

--- cs336_systems/ddp.py
+++ cs336_systems/ddp.py
@@ -5,13 +5,19 @@ import torch.distributed as dist
 class DDP(nn.Module):
     def __init__(self, module: nn.Module):
         '''
-        作为module的上层DDP的wrapper类
+        作为module的上层DDP的wrapper类。
+        1. 把rank0的参数广播出去
+        2. 初始化参数梯度更新以后的hook函数，从而实现overlap的reduce和backward
         '''
         super().__init__()
         for parameter in module.parameters():
             with torch.no_grad():
                 dist.broadcast(parameter, 0)
+                if parameter.requires_grad == True:
+                    handle = parameter.register_post_accumulate_grad_hook(self.post_acc_grad_hook)
         self.module = module
+
+        self.handle_para_list = []
 
     def forward(self, x):
         return self.module(x)
@@ -21,20 +27,19 @@ class DDP(nn.Module):
         backward 之后 step 之前，把所有的device的梯度平均
         '''
         world_size = dist.get_world_size()
-        grad_list = []
         
         with torch.no_grad():
-            for parameter in self.module.parameters():
-                if parameter.grad is not None:
-                    grad_list.append(parameter.grad)
-            
-            flat_grad = torch._utils._flatten_dense_tensors(grad_list)
-            dist.all_reduce(flat_grad, async_op=False)
-            flat_grad /= world_size
-            reduced_grad_list = torch._utils._unflatten_dense_tensors(flat_grad, grad_list)
+            for handle, parameter in self.handle_para_list:
+                handle.wait()
+                parameter.grad /= world_size 
+        
+        # 清空handle list
+        self.handle_para_list = []
 
-            grad_idx = 0
-            for parameter in self.module.parameters():
-                if parameter.grad is not None:
-                    parameter.grad = reduced_grad_list[grad_idx]
-                    grad_idx += 1
+    def post_acc_grad_hook(self, parameter: nn.Parameter):
+        '''
+        hook 函数，在当前参数梯度backward结束时调用
+        '''
+        with torch.no_grad():
+            handle = dist.all_reduce(parameter.grad, async_op=True)
+            self.handle_para_list.append((handle, parameter))
